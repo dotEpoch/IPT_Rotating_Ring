07f2bcf592596c37b26b9179c401c816cd0727c2
Vincent Aucoin <vincent.aucoin@mail.mcgill.ca>
2025-03-18 17:24:23 -0400

    Implemented Servo motor control for magnet launch procedure

--- sketch_mar17a/sketch_mar17a.ino
+++ sketch_mar17a/sketch_mar17a.ino
@@ -1,28 +1,73 @@
+/*
+--- Rotating Magnetic Ring Experimental Setup  ---
+Authors: Vincent Aucoin, Didar Sedghi
+Description: 
+*/
+
+// Packages //
+#include <Servo.h>
+
+// Initialization //
 int inPin = A0;
-int voltPin = 5;
+int photoVoltPin = 4;
+int LEDvoltPin = 2;
+int servoPin = 9;
+float startT = millis();
+Servo myServo; //new  servo objet
 
 
+/*~~~~~~~~~~~~~~~~ Methods ~~~~~~~~~~~~~~~~*/
+void pingLED(){
+  float lightIntensity = analogRead(inPin);
+
+  Serial.print(lightIntensity);
+  Serial.print(',');
+  Serial.print((millis() - startT)/1000.0);
+  Serial.print(',');
+  Serial.println(0);
+}
+
+void getSteps() {
+  int val;
+
+  while(Serial.available() > 0) {
+    val = Serial.parseInt();
+    if (val != 0) {
+      myServo.write(val);
+      Serial.print("> INITIATE MAGNET DESCENT ! ! ! AT T=");
+      Serial.print((millis() - startT + 1000)/1000.0); 
+      Serial.print(',');
+      Serial.println(0);
+    }
+  }
+}
+
+/*~~~~~~~~~~~~~~~~ RUN ~~~~~~~~~~~~~~~~*/
 void setup() {
-  // put your setup code here, to run once:
+  // Pins //
   pinMode(inPin, INPUT);
-  pinMode(voltPin, OUTPUT);
+  pinMode(LEDvoltPin, OUTPUT);
+  digitalWrite(LEDvoltPin, HIGH);
+  pinMode(photoVoltPin, OUTPUT);
+  digitalWrite(photoVoltPin, HIGH);
 
-  digitalWrite(voltPin, HIGH);
+  // Servo //
+  myServo.attach(servoPin);
+  myServo.write(180);
+  delay(5);
+  // myServo.write(180);
+  // delay(5);
 
+  // Serial //
   Serial.begin(115200);
 }
 
-float startT = millis();
+
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
 
-
-  float lightIntensity = analogRead(inPin);
-  Serial.print(lightIntensity);
-  Serial.print(',');
-  Serial.print(millis() - startT);
-  Serial.print(',');
-  Serial.print(0);
-  Serial.println();
-  delay(10);
+  getSteps();
+  pingLED();
+  delay(5);
 }
